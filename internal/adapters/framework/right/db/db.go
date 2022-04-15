5c95152b69b10c7c65d0afb29ef67983c41d3d45
malakanov <sergey.malakanov@gmail.com>
2022-04-15 22:20:50 +0500

    add addHistory

--- internal/adapters/framework/right/db/db.go
+++ internal/adapters/framework/right/db/db.go
@@ -2,10 +2,12 @@ package db
 
 import (
 	"database/sql"
-	sq "github.com/Masterminds/squirrel"
-	_ "github.com/go-sql-driver/mysql"
+	"fmt"
 	"log"
 	"time"
+
+	sq "github.com/Masterminds/squirrel"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type Adapter struct {
@@ -22,4 +24,26 @@ func NewAdapter(driverName, datasource string) (*Adapter, error) {
 		log.Fatal("ping fail")
 	}
 	return &Adapter{db: db}, nil
+}
+
+func (da Adapter) CloseDbConnection() {
+	err := da.db.Close()
+	if err != nil {
+		fmt.Println("close db error")
+	}
+}
+
+func (da Adapter) AddHistory(answer int32, operation string) error {
+	querystring, args, err := sq.Insert("arith_history").Columns("date", "answer", "operation").Values(time.Now(), answer, operation).ToSql()
+	if err != nil {
+		return err
+	}
+
+	_, err = da.db.Exec(querystring, args)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
